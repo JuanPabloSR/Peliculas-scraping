e138ec2abaf02f12db96d0395c5fed7d14712e7f
Juan Pablo Sánchez Rey <juanpablosanchezrey22@gmail.com>
2022-01-19 17:11:45 -0500

    primer commit

---
+++ src/main/java/co/com/sofka/peliculasscraping/PeliculasScrapingApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.peliculasscraping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeliculasScrapingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeliculasScrapingApplication.class, args);
+	}
+
+}
